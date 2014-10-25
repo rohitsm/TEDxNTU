d09ffedeae48426d9c265ce930830424e1a84c45
Rohit Menon <rohitsm@gmail.com>
2014-10-25 22:36:26 +0800

    fixed encoding issues

--- buildXML.py
+++ buildXML.py
@@ -1,30 +1,34 @@
 import csv
 
-csvFile = ''	# add name of input CSV file here
-xmlFile = ''	# add name of output XML file here	
+csvFile = 'Speech_2008.csv'
+xmlFile = 'Speech_2008.xml'
 
+
+def removeNonAscii(s): 
+	return "".join(i for i in s if ord(i)<128)
 
 rowNum = 0
 with open(csvFile, 'rU') as x:
 	csvData = csv.reader(x, delimiter=',')
 	
 	xmlData = open(xmlFile, 'w')
-	xmlData.write('<?xml version="1.0"?>' + "\n")
-	xmlData.write('<comments>' + '\n')	#top level tag
+	xmlData.write(removeNonAscii('<?xml version="1.0"?>' + "\n"))
+	xmlData.write(removeNonAscii('<comments>' + '\n'))	#top level tag
 	
 	for line in csvData:
 		if rowNum != 0:
 			# XML structure:
 			# <comment id="1" author="John_Smith">Blah blah </comment>
 			
-			xmlData.write(	'    ' + # preserve indentation
-							'<comment ' +
-							'id="' + str(rowNum) + '" ' +   
-							'author="' + line[1] + '">' +
-							line[2].replace('\n', ' ') + 		
-							'</comment>' + '\n')
+			comment = 	'    ' + \
+						'<comment ' + \
+						'id="' + str(rowNum) + '" ' + \
+						'author="' + line[1] + '">' + \
+						line[2].replace('\n', ' ') + \
+						'</comment>' + '\n'
+			xmlData.write(removeNonAscii(comment).decode('UTF-8'))
 		rowNum += 1	
 
-	xmlData.write('</comments>')
+	xmlData.write(removeNonAscii('</comments>'))
 	xmlData.close()
 		
